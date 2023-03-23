d0303b6bb780056f9bc4dc3563d53f5e9295ec7d
MKim1871 <mkim30231871@gmail.com>
2023-03-23 16:20:31 -0700

    Laptop Commit 1

--- GITA 4 game/Assets/Scripts/PlayerMotor.cs
+++ GITA 4 game/Assets/Scripts/PlayerMotor.cs
@@ -6,7 +6,10 @@ public class PlayerMotor : MonoBehaviour
 {
 	private CharacterController controller;
 	private Vector3 playerVelocity;
-	public float speed = 5f;
+	private bool isGrounded;
+	private float speed = 5f;
+	private float gravity = -9.8f;
+	private float jumpHeight = 3f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,16 +19,33 @@ public class PlayerMotor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        isGrounded = controller.isGrounded;
     }
 	
 	//Receives inputs from InputManager.cs and applies them to controller
 	public void ProcessMove(Vector2 input)
 	{
 		Vector3 moveDirection = Vector3.zero;
-		moveDirection.x = input.x;
 		//allows left or right movement, is z and not y because y is up
+		moveDirection.x = input.x;
 		moveDirection.z = input.y;
 		controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+		playerVelocity.y += gravity * Time.deltaTime;
+		
+		if(isGrounded && playerVelocity.y < 0)
+		{
+			playerVelocity.y = -2f;
+		}
+		
+		controller.Move(playerVelocity * Time.deltaTime);
+		Debug.Log(playerVelocity.y);
+	}
+	
+	public void Jump()
+	{
+		if(isGrounded)
+		{
+			playerVelocity.y = Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+		}
 	}
 }
