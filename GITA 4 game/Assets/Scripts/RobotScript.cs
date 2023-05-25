936c4e7f3238cf1218686cac9ec04ee8e1cf75b3
MKim1871 <mkim30231871@gmail.com>
2023-05-24 18:33:59 -0700

    robot 2

--- GITA 4 game/Assets/Scripts/RobotScript.cs
+++ GITA 4 game/Assets/Scripts/RobotScript.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class RobotScript : MonoBehaviour
 {
-	public GameObject 
+	public GameObject playerPrefab;
+	public GameObject robotPrefab;
+	
     // Start is called before the first frame update
     void Start()
     {
-        
+		
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+		Vector3 distanceFromPlayer = playerPrefab.transform.position - transform.localPosition;
+		float angleComparedToPlayer = Vector3.Angle(transform.forward, distanceFromPlayer);
+		
+		if (angleComparedToPlayer < 90f * 0.5f && distanceFromPlayer.magnitude < 10f)
+		{
+			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(distanceFromPlayer), 10f * Time.deltaTime);
+			
+	        transform.Translate(Vector3.forward * Time.deltaTime);
+		}	
+		
     }
 }
